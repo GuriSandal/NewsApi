06ee5d33aacbb784afa52de023dc59d8a34ec766
GuriSandal <sandalgurjant@gmail.com>
2020-06-09 09:02:14 +0530

    bug fix

--- news_api_app/views.py
+++ news_api_app/views.py
@@ -20,7 +20,7 @@ class StateList(APIView):
     """
     def get(self, request):
         response_data = {}
-        response_data["headlineText"] = Headline.objects.get(headlineId=2).headlineText
+        response_data["headlineText"] = Headline.objects.get(headlineId=1).headlineText
         response_data["stateList"] = StateSerializer(State.objects.order_by("stateName"), context={"request": request}, many=True).data
         return Response(response_data, status=status.HTTP_200_OK)
 
